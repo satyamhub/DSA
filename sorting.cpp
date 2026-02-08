4ce20074356a071f3e1a143afaa60fe877d95bd6
satyamhub <44332322+satyamhub@users.noreply.github.com>
2026-02-08 17:25:54 +0530

    DAY 4

---
+++ sorting.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void sorting(){
+
+    string alp1="abcdefghijklmnopqrstuvwxyz";
+
+    string a[27]={"satyam" , "amit" ,"dev"};
+    for(int i=0; i<=2; i++){
+        for(int j=0; j<27; j++){
+            if(a[i][0]==alp1[i]){
+                 string ab[3]= {a['i']};
+            }
+           
+           
+        
+    }
+    }
+    
+
+
+    // cout<<sizeof(b);
+    
+    // for(int i=0; i<27; i++){
+        
+    //        if(a[0]==alp1[i] or b[0]==alp1[i] or c[0]==alp1[i]){
+    //            cout<<alp1[i]<<endl;
+        
+    // }
+    // }
+
+
+    
+
+
+
+
+     
+}
+
+int main(){
+    char ch='a';
+    char ch2=(char)(ch+1);
+    cout<<ch2;
+    sorting();
+   
+ 
+
+ 
+
+    return  0;
+}
