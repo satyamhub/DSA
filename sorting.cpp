0225e3d8796e274b275593b871805caef37cba45
satyamhub <44332322+satyamhub@users.noreply.github.com>
2026-02-27 09:35:53 +0530

    Insertion Sort

--- sorting.cpp
+++ sorting.cpp
@@ -9,11 +9,13 @@ void selection_sort(int arr[], int n){
     * space Complexity: O(1) as it is an in-place sorting algorithm.        
     
     */
+    int isSwapped=0;
     for(int i=0; i<=n-2; i++){
        int mini=i;
        for(int j=i; j<=n-1; j++){
         if(arr[j]<arr[mini]){
             mini=j;
+            isSwapped++;
         }
         
        }
@@ -23,44 +25,82 @@ void selection_sort(int arr[], int n){
        
 
     }
+    cout<<"No. of Swapping: "<<isSwapped<<endl;
 
 }
 
 /*
      * Bubble Sort
     * time Complexity: O(n square) in worst case and O(n) in best case when the array is already sorted.
-    * space Complexity: O(1) as it is an in-place sorting algorithm.
-    * to optimize the bubble sort, we can add a flag to check if any swapping occurs in the inner loop. If no swapping occurs, it means the array is already sorted and we can break out of the loop early, reducing the time complexity to O(n) in the best case.
-    * Here is the optimized version of bubble sort:
-    * void optimized_bubble_sort(int arr[], int n){
-    *    bool swapped;
-    *   for(int i=0; i<n-1; i++){
-    *    swapped=false;
-    *   for(int j=0; j<n-1-i; j++){
-    *   if(arr[j]>arr[j+1]){
-    *      int temp=arr[j];
-    *     arr[j]=arr[j+1];
-    *    arr[j+1]=temp;
-    *   swapped=true;
-    *  }
-    * }
-    *  if(!swapped){
-    *   break;
-    * }
-    * 
+    
      
 */           
 void bubble_sort(int arr[], int n){
+    int isSwapped=0;
     for(int i=0; i<n-1; i++){
         for(int j=0; j<n-1-i; j++){
             if(arr[j]>arr[j+1]){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
+                isSwapped=isSwapped+1;
             }
         }
+        cout<<"No. of Swapping: "<<isSwapped<<endl;
+        if(isSwapped=0){
+           break; 
+        }
+        
     }
+    
 }
+
+
+void insertion_sort(int arr[], int n){
+
+    /*
+    * Time Complexity: O(n square) in worst case( 5 4 3 2 1) and avg case
+      * and O(n) is best case: 1 2 3 4 5
+
+
+     
+    * Example: 12 11 23 4 1
+
+
+     11 12 23 4 1
+     11 12 4 23 1
+     11 4  12 23 1
+     4 11 12 23 1
+
+     4 11 12 1 23
+     4 11 1 12 23
+     4 1 11 12 23
+     1 4 11 12 23
+
+     * no of swapping=8
+    
+    */
+   int isSwaped=0;
+    for(int i=0; i<=n-1; i++){
+       
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j] ) {
+                int temp=arr[j-1];
+                arr[j-1]=arr[j];
+                arr[j]=temp;
+                j--;
+                isSwaped++;
+                
+    }cout<<"No. of Swapping: "<<isSwaped<<endl;
+    }
+    
+    
+ 
+}
+
+
+
+
 
 
 int main(){
@@ -68,7 +108,9 @@ int main(){
     cin>>n;
     int arr[n];
     for(int i=0; i<n; i++) cin>>arr[i];
-    bubble_sort(arr, n);
+   // selection_sort(arr, n);
+   //bubble_sort(arr, n);
+    insertion_sort(arr, n);
 
     for(int i=0; i<n; i++) {
         cout<<arr[i]<<" ";
