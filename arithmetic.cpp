940623091f124c5ea4ef4810f1c92b3d70dca6d8
satyamhub <44332322+satyamhub@users.noreply.github.com>
2026-02-28 00:31:38 +0530

    Complete Prime

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -38,6 +38,19 @@ void PrimeNumber(){
          }
 }
 
+bool isPrime(int n){
+  //*Best Optimized version of Prime Number check
+  //* 6K+-1 form
+    if(n < 2) return false;
+    if(n == 2 || n == 3) return true;
+    if(n % 2 == 0 || n % 3 == 0) return false;
+
+    for(int i = 5; i * i <= n; i += 6){
+        if(n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
 int hcf(){
    //Solution 1
    // time complexity O(min(x,y))
