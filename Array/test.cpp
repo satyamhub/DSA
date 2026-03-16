2fb4937ce4b1e95eb25cf23f7623260a5aeab65b
satyamhub <44332322+satyamhub@users.noreply.github.com>
2026-03-16 22:12:45 +0530

    Array

--- Array/test.cpp
+++ Array/test.cpp
@@ -1,30 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int t; // Variable to store the number of test cases
-    cin >> t; // Read the number of test cases
-    while (t--)
-    {
-        long long n, k; // Variables to store the number of boxes and the maximum length of subarray that can be reversed
-        cin >> n >> k; // Read the values of n and k
-        vector<long long> a(n); // Vector to store the numbers on the boxes
-        for (int i = 0; i < n; i++) // Loop to read the numbers on the boxes
-            cin >> a[i]; // Read each number into the vector
-        // Inputs are read
+int main() {
 
-        vector<long long> copy_a = a; // Create a copy of the original array
-        sort(copy_a.begin(), copy_a.end()); // Sort the copied array in non-decreasing order
+    int n;
+    cin >> n;
 
-        // Check if the original array is already sorted or if k > 1
-        if (copy_a == a || k > 1)
-            cout << "YES" << endl; // If true, print "YES"
-        else
-            cout << "NO" << endl; // Otherwise, print "NO"
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
     }
-    return 0; // End of the program
-}
 
-// Time Complexity (TC): O(nlogn) = O(100log2(100)) = 100*7 = 700
-// Space Complexity (SC): O(n)
+    unordered_map<int,int> mp;
+    mp[0] = -1;
+
+    int sum = 0;
+    int left = -1, right = -1;
+    int maxLen = 0;
+
+    for(int i = 0; i < n; i++){
+
+        if(arr[i] == 1) sum += 1;
+        else sum -= 1;
+
+        if(mp.find(sum) != mp.end()){
+
+            int len = i - mp[sum];
+
+            if(len > maxLen){
+                maxLen = len;
+                left = mp[sum] + 1;
+                right = i;
+            }
+
+        } else {
+            mp[sum] = i;
+        }
+    }
+
+    cout << left << " to " << right;
+}
