6913d5945993a9eaf814e1838a62db7fdd73fea1
satyamhub <44332322+satyamhub@users.noreply.github.com>
2026-03-24 19:32:57 +0530

    update

--- 04_Sorting/bubble_sort.cpp
+++ 04_Sorting/bubble_sort.cpp
@@ -19,35 +19,43 @@ Approach: Repeatedly swap adjacent out-of-order elements
 - Space: O(1)
 */
 
-void bubbleSort(vector<int> &arr) {
+void bubbleSort(vector<int> &arr)
+{
     int n = static_cast<int>(arr.size());
 
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; i++)
+    {
         bool swapped = false;
-        for (int j = 0; j < n - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
         }
-        if (!swapped) {
+        if (!swapped)
+        {
             break;
         }
     }
 }
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
 
     vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
 
     bubbleSort(arr);
 
-    for (int value : arr) {
+    for (int value : arr)
+    {
         cout << value << ' ';
     }
     cout << '\n';
