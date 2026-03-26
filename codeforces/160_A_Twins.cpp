65026d4980911f9b4696489aa086b4ae98f181eb
satyamhub <44332322+satyamhub@users.noreply.github.com>
2026-03-26 23:16:06 +0530

    Adds solution for Codeforces 160A Twins
    Provides a C++ solution to find the minimum number of coins required to achieve a sum greater than the remaining sum. Includes local test configuration for problem validation.

---
+++ codeforces/160_A_Twins.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int totalsum = 0;
+    vector<int> coins(n);
+    // input
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        coins[i] = x;
+        totalsum += x;
+    }
+    // decending order sorting
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (coins[j] > coins[i])
+            {
+                int temp = coins[i];
+                coins[i] = coins[j];
+                coins[j] = temp;
+            }
+        }
+    }
+    // checking
+    int mysum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        mysum += coins[i];
+        int remain = totalsum - mysum;
+        if (mysum > remain)
+        {
+            cout << i + 1;
+            break;
+        }
+    }
+}
