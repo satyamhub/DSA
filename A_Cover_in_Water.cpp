260bd16025f4f57274794c1fae7750970c041ece
satyamhub <44332322+satyamhub@users.noreply.github.com>
2026-03-28 23:38:22 +0530

    update

---
+++ A_Cover_in_Water.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<string> s(n);
+        for (int i = 0; i < n; i++)
+        {
+            char ch;
+            cin >> ch;
+            s[i] = ch;
+        }
+        int dot = 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == ".")
+            {
+                ans += 2;
+            }
+        }
+        cout << n - ans << endl;
+    }
+}
--- codeforces/2204_B_Right Maximum.cpp
+++ codeforces/2204_B_Right Maximum.cpp
@@ -1,41 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
     cin >> t;
     while (t--)
     {
         int n;
         cin >> n;
-        int arr[n];
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        vector<int> rightmostMaxPos(n);
+        int maxVal = -1, pos = -1;
         for (int i = 0; i < n; i++)
         {
-            int x;
-            cin >> x;
-            arr[i] = x;
+            if (a[i] > maxVal)
+            {
+                maxVal = a[i];
+                pos = i;
+            }
+            else if (a[i] == maxVal)
+            {
+                pos = i;
+            }
+            rightmostMaxPos[i] = pos;
         }
+
         int cnt = 0;
-        int left = n - 1;
-        while (left > 0)
+        int i = n - 1;
+        while (i >= 0)
         {
-
-            while (arr[left] < arr[left - 1])
-            {
-                left--;
-            }
-            if (arr[left] >= arr[left - 1])
-            {
-                if (left == 0)
-                {
-                    cnt++;
-                    break;
-                }
-                cnt++;
-                left--;
-            }
+            i = rightmostMaxPos[i] - 1;
+            cnt++;
         }
-
-        cout << cnt << endl;
+        cout << cnt << "\n";
     }
+    return 0;
 }
