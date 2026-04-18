3a05423836c9f35d365e3525503e86d0d45ec072
satyamhub <44332322+satyamhub@users.noreply.github.com>
2026-04-18 22:48:09 +0530

    Implement reverse number logic; update function to accept input and modify test cases accordingly

--- Revision/01_Basics/reverse_number.cpp
+++ Revision/01_Basics/reverse_number.cpp
@@ -13,12 +13,40 @@ Output: 4321
 Approach Hint: Extract digits using modulo and build reversed number
 */
 
-void solve() {
-    // Write your solution here
+/*
+---Brute---------
+I will Run while Loop till n not becomes 0.
+I will extract lastdigit with n%10
+And i will start making number from all last digit.
+And At the end i will return the rev.
+
+TC: O(log10(N))
+SC: O(1)
+*/
+
+/*
+---------Optimal-----
+
+
+
+
+*/
+void solve(int n) {
+    int rev = 0;
+    while (n != 0) {
+        int lastdigit = n % 10;
+        rev = rev * 10 + lastdigit;
+        n /= 10;
+    }
+    cout << rev << endl;
 }
+
 
 int main() {
     // Test cases
-    solve();
+    solve(1234);
+    solve(2110);
+    solve(0);
+    solve(120);
     return 0;
 }
